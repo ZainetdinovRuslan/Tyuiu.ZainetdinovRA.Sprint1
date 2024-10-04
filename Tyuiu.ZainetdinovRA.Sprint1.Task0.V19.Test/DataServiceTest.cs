e61a6eeb0f1fb70342effc607313ff2a4939012e
Tyuiu.ZainetdinovRA@outlook.com <rosiper123@icloud.com>
2024-10-05 01:19:01 +0500

    Add project files.

---
+++ Tyuiu.ZainetdinovRA.Sprint1.Task0.V19.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+using Tyuiu.ZainetdinovRA.Sprint1.Task0.V19.Lib;
+
+namespace Tyuiu.ZainetdinovRA.Sprint1.Task0.V19.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            var result = ds.Calculate();
+            Assert.AreEqual(10, result);
+        }
+    }
+}
