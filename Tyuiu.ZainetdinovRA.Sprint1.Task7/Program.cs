50d5f3906f113b56271ec131a032642aa2d6ee30
Tyuiu.ZainetdinovRA@outlook.com <rosiper123@icloud.com>
2024-10-19 02:18:45 +0500

    Task7

---
+++ Tyuiu.ZainetdinovRA.Sprint1.Task7/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Text;
+using Tyuiu.ZainetdinovRA.Sprint1.Task7.V8.Lib;
+
+namespace Tyuiu.ZainetdinovRA.Sprint1.Task7.V8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+
+        {
+            DataService ds = new DataService(); 
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.Title = "Спринт #1 | Выполнил: Zainetdinov R. A. | ИИПБ-24-2";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях                 *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #8                                                              *");
+            Console.WriteLine("* Выполнил: Зайнетдинов Руслан Александрович | ИИПБ-24-2                  *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая вычисляет математическое выражение по       *");
+            Console.WriteLine("* исходным значениям данных, вводимых пользователем. Ответ округлите до 3 *");
+            Console.WriteLine("* знаком после запятой.                                                   *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double x, y;
+
+            Console.Write("Введите значение x: ");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            var result = ds.Calculate(x, y);
+
+            Console.WriteLine();
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ                                                               *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine($"При x = {x} и y = {y} ответ будет равен {result}");
+        }
+    }
+}
