b0ffa3b07b0ab74262e9a072eb4c0eafa9931cb0
Tyuiu.ZainetdinovRA@outlook.com <rosiper123@icloud.com>
2024-10-20 03:23:18 +0500

    Task7

---
+++ Tuyiu.ZainetdinovRA.Sprint1.Task7.V8.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+using Tyuiu.ZainetdinovRA.Sprint1.Task7.V8.Lib;
+
+namespace Tyuiu.ZainetdinovRA.Sprint1.Task7.V8.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            int x = 1;
+            int y = 1;
+            double wait = 1.517;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
