0a6934d0435e53b2f61b77d719fd02ff29e39968
Tyuiu.ZainetdinovRA@outlook.com <rosiper123@icloud.com>
2024-10-23 00:54:38 +0500

    Task1

--- Tyuiu.ZainetdinovRA.Sprint1.Task1.V24/Program.cs
+++ Tyuiu.ZainetdinovRA.Sprint1.Task1.V24/Program.cs
@@ -1,4 +1,5 @@
-﻿using Tyuiu.ZainetdinovRA.Sprint1.Task1.V24.Lib;
+﻿using System.Text;
+using Tyuiu.ZainetdinovRA.Sprint1.Task1.V24.Lib;
 
 namespace Tyuiu.ZainetdinovRA.Sprint1.Task1.V24
 {
@@ -6,21 +7,39 @@ namespace Tyuiu.ZainetdinovRA.Sprint1.Task1.V24
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+
+            Console.OutputEncoding = Encoding.UTF8;
             Console.Title = "Спринт #1 | Выполнил: Zainetdinov R. A. | ИИПБ-24-2";
             Console.WriteLine("***********************************************************************");
-            Console.WriteLine("* Спринт #0                                                           *");
-            Console.WriteLine("* Тема: Создания итогового решения по спринту                         *");
-            Console.WriteLine("* Задание #7                                                          *");
-            Console.WriteLine("* Вариант #0                                                          *");
+            Console.WriteLine("* Спринт #1                                                           *");
+            Console.WriteLine("* Тема: Организация ввода/вывода в консольных приложениях             *");
+            Console.WriteLine("* Задание #1                                                          *");
+            Console.WriteLine("* Вариант #24                                                         *");
             Console.WriteLine("* Выполнил: Зайнетдинов Руслан Александрович | ИИПБ-24-2              *");
             Console.WriteLine("***********************************************************************");
             Console.WriteLine("* УСЛОВИЕ:                                                            *");
-            Console.WriteLine("* Написать консольную программу на C#, которая суммирует значения     *");
-            Console.WriteLine("* двух одинаковых массивов по длине                                   *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные     *");
+            Console.WriteLine("* данные, вычисляет результат по формуле (1-x)/(2+y) и печатает его   *");
+            Console.WriteLine("* на экране.                                                          *");
             Console.WriteLine("*                                                                     *");
             Console.WriteLine("***********************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                    *");
             Console.WriteLine("***********************************************************************");
+
+            double x, y;
+            Console.Write("Введите значение x: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите значение y: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            var result = ds.Calculate(x, y);
+            Console.WriteLine("***********************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                    *");
+            Console.WriteLine("***********************************************************************");
+
+            Console.WriteLine($"Выражение (1-x)/(2+y), при значениях x = {x} и y = {y} равно: {result}"); 
         }
     }
 }
